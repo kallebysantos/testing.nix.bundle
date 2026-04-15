3ca6bc67e47b56708b1c0ad46ce3c71e5de8ccf7
Kalleby Santos <kalleby_santos@hotmail.com>
2026-04-15 23:16:40 +0100

    feat: use crate2nix

---
+++ _flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "My Rust CLI";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.nix-app = pkgs.callPackage ./nix/package.nix { };
+        defaultPackage = self.packages.${system}.nix-app;
+      });
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use ndarray::Array4;
-use ort::execution_providers::CPUExecutionProvider;
-use ort::session::Session;
+// use ndarray::Array4;
+// use ort::execution_providers::CPUExecutionProvider;
+// use ort::session::Session;
 
 fn main() {
-    let session = Session::builder()
-        .unwrap()
-        .with_execution_providers([CPUExecutionProvider::default().build()])
-        .unwrap();
+    //  let session = Session::builder()
+    //      .unwrap()
+    //      .with_execution_providers([CPUExecutionProvider::default().build()])
+    //      .unwrap();
 
-    // Example input: batch of 1 image, 3 channels, 224x224 (e.g. ResNet)
-    let input = Array4::<f32>::zeros((1, 3, 224, 224));
+    //  // Example input: batch of 1 image, 3 channels, 224x224 (e.g. ResNet)
+    //  let input = Array4::<f32>::zeros((1, 3, 224, 224));
 
-    println!("Hello, world! {input:?}");
+    // println!("Hello, world! {input:?}");
+    println!("Hello, world!");
 }
